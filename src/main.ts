310bfe2fcde6547270155fe78621b388d8a95536
shohag1102 <saikathossain1102@gmail.com>
2025-01-03 23:40:08 +0600

    update main.ts

--- src/main.ts
+++ src/main.ts
@@ -2,17 +2,9 @@
 
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import * as express from 'express';
-import { ExpressAdapter } from '@nestjs/platform-express';
 
 async function bootstrap() {
-  const server = express();
-  // const app = await NestFactory.create(AppModule);
-  const app = await NestFactory.create(AppModule, new ExpressAdapter(server));
-  // await app.listen(process.env.PORT ?? 3000);
-  await app.init();
-
-  return server;
+  const app = await NestFactory.create(AppModule);
+  await app.listen(process.env.PORT ?? 3000);
 }
-// bootstrap();
-bootstrap().then((server) => server.listen(3000));
+bootstrap();
