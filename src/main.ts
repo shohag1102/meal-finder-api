2d83b3eacea06a4fd7713f2c273f3a3d43c479c1
shohag1102 <saikathossain1102@gmail.com>
2025-01-03 23:58:22 +0600

    enable cors

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,11 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: 'https://subtle-chimera-b14739.netlify.app/', // Replace with your frontend's URL
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    allowedHeaders: 'Content-Type, Accept',
+  });
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
